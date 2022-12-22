2adee87a679c55be203858b461fce33e2347445e
abduljabbar15 <abduljabber1532002@gmail.com>
2022-12-22 15:54:38 +0600

    add

--- src/Components/Home/WelcomeBanner.js
+++ src/Components/Home/WelcomeBanner.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import welcomeImage from '../../resource/welcome.png';
+import welcomeImage from '../../resource/1.png';
 const WelcomeBanner = () => {
 return (
 <>
     <div className='flex flex-col relative z-20 items-center justify-center text-white '>
         <div className="container  absolute z-30">
-            <h2 className='  md:text-5xl text-2xl  font-semibold md:text-left text-center'>Best Polytechnic Institute In
-                Gazipur</h2>
-            <p className='md:text-2xl text-md md:mb-4'>At SPDI, you'll explore new ideas while learning more about
-                yourself. </p>
+            <h2 className='  md:text-5xl text-2xl  font-semibold md:text-left text-center'>Modern Industry for Skills Development </h2>
+            <p className='md:text-2xl font-light text-md md:mb-4'>We make/develop/produce any Engg. product/system/spare part for professional use</p>
 
             <Link to={'/course'} className='text-blue-500 py-2 px-4 uppercase font-medium '>Find your department</Link>
         </div>
