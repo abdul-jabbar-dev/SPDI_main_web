2873f4a5b161a5d76a693275051d735bc882644c
abduljabbar15 <abduljabber1532002@gmail.com>
2023-01-19 16:07:10 +0600

    server image not clear from chng img in landing page

--- src/Components/Home/WelcomeBanner.js
+++ src/Components/Home/WelcomeBanner.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from 'react'; 
 import { Link } from 'react-router-dom';
-import welcomeImage from '../../resource/1.png';
-const WelcomeBanner = () => {
-return (
-<>
-    <div className='flex flex-col relative z-20 items-center justify-center text-white '>
-        <div className="container  absolute z-30">
-            <h2 className='  md:text-5xl text-2xl  font-semibold md:text-left text-center'>Modern Industry for Skills Development </h2>
-            <p className='md:text-2xl font-light text-md md:mb-4'>We make/develop/produce any Engg. product/system/spare part for professional use</p>
+ 
+const WelcomeBanner = ({ pageData }) => { 
+    return (
+        <>
+            <div className='flex flex-col relative z-20 items-center justify-center text-white '>
+                <div className="container  absolute z-30">
+                    <h2 className='  md:text-5xl text-2xl  font-semibold md:text-left text-center'>{pageData.title}</h2>
+                    <p className='md:text-2xl font-light text-md md:mb-4'>{pageData.descreption}</p>
 
-            <Link to={'/course'} className='text-blue-500 py-2 px-4 uppercase font-medium '>Find your department</Link>
-        </div>
-        <img src={welcomeImage } className="w-full opacity-80" alt="" />
-    </div>
-</>
-);
+                    <Link to={'/course'} className='text-blue-500 py-2 px-4 uppercase font-medium '>Find your department</Link>
+                </div>
+                <img src={process.env.REACT_APP_ROOT_URL+'/'+pageData.headingBackground} className="w-full opacity-80" alt="" />
+            </div>
+        </>
+    );
 };
 
 export default WelcomeBanner;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,29 +7,39 @@ import Slider from '../Components/Home/Slider';
 import Footer from '../globalComponents/Footer';
 import Navbar from '../globalComponents/Navbar';
 import WelcomeBanner from '../Components/Home/WelcomeBanner';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import mediaApi from './../Api/media';
-const CustomHeader = () => {
-    return (<Helmet>
-        <title>Skills and Professional Development Industry</title>
-    </Helmet>)
-}
+
 
 
 const Home = () => {
-    const [state, setState] = useState(mediaApi || []) 
+    const [pageData, setPageData] = useState([])
+    const [isLoding, setIsLoding] = useState(!!pageData)
+    const [state, setState] = useState(mediaApi || [])
+
+    useEffect(() => {
+        setIsLoding(true)
+        fetch(`http://localhost:5000/dynamicpage`)
+            .then(res => res.json())
+            .then(res => {
+                setPageData(res)
+                setIsLoding(false)
+            })
+    }, []) 
     return (
         <>
-            <CustomHeader></CustomHeader>
 
             <body className="bg-gray-100">
                 <Navbar></Navbar>
-
-                <WelcomeBanner></WelcomeBanner>
-                <ShortBanner></ShortBanner>
-                <Slider></Slider><br /><br />
-                <Bannar></Bannar>
-                <Events state={state}></Events>
+                {
+                    !isLoding ? <>
+                        <WelcomeBanner pageData={pageData.find(o=>o.role==='heading')} ></WelcomeBanner>
+                        <ShortBanner pageData={pageData.find(o=>o.role==='topfeature')} ></ShortBanner>
+                        <Slider></Slider><br /><br />
+                        <Bannar pageData={pageData.find(o=>o.role==='secfeature')}></Bannar>
+                        <Events state={state}></Events>
+                    </> : 'loding'
+                }
             </body>
             <Footer ></Footer>
         </>
