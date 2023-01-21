76b47aac1e6feaa0d5ab75c4d7dbd8e8853519c4
khalidumar29 <dreamiwoarld.online@gmail.com>
2023-01-21 13:41:28 +0600

    home page error done and spiner add

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,50 +1,57 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import Events from '../Components/Home/Events';
-import ShortBanner from '../Components/Home/ShortBanner';
-import Bannar from '../Components/Home/Bannar';
-import Slider from '../Components/Home/Slider';
-import Footer from '../globalComponents/Footer';
-import Navbar from '../globalComponents/Navbar';
-import WelcomeBanner from '../Components/Home/WelcomeBanner';
-import { useState, useEffect } from 'react';
-import mediaApi from './../Api/media';
-
-
+import React from "react";
+import Events from "../Components/Home/Events";
+import ShortBanner from "../Components/Home/ShortBanner";
+import Bannar from "../Components/Home/Bannar";
+import Slider from "../Components/Home/Slider";
+import Footer from "../globalComponents/Footer";
+import Navbar from "../globalComponents/Navbar";
+import WelcomeBanner from "../Components/Home/WelcomeBanner";
+import { useState, useEffect } from "react";
+import mediaApi from "./../Api/media";
+import Loading from "../globalComponents/Loading";
 
 const Home = () => {
-    const [pageData, setPageData] = useState([])
-    const [isLoding, setIsLoding] = useState(!!pageData)
-    const [state, setState] = useState(mediaApi || [])
+  const [pageData, setPageData] = useState([]);
+  const [isLoding, setIsLoding] = useState(!!pageData);
+  const [state, setState] = useState(mediaApi || []);
 
-    useEffect(() => {
-        setIsLoding(true)
-        fetch(`http://localhost:5000/dynamicpage`)
-            .then(res => res.json())
-            .then(res => {
-                setPageData(res)
-                setIsLoding(false)
-            })
-    }, []) 
-    console.log(pageData)
-    return (
-        <>
+  useEffect(() => {
+    setIsLoding(true);
+    fetch(`http://localhost:5000/dynamicpage`)
+      .then((res) => res.json())
+      .then((res) => {
+        setPageData(res);
+        setIsLoding(false);
+      });
+  }, []);
 
-            <body className="bg-gray-100">
-                <Navbar></Navbar>
-                {
-                    !isLoding ? <>
-                        <WelcomeBanner pageData={pageData.find(o=>o.role==='heading')} ></WelcomeBanner>
-                        <ShortBanner pageData={pageData.find(o=>o.role==='topfeature')} ></ShortBanner>
-                        <Slider></Slider><br /><br />
-                        <Bannar pageData={pageData.find(o=>o.role==='secfeature')}></Bannar>
-                        <Events state={state}></Events>
-                    </> : 'loding'
-                }
-            </body>
-            <Footer ></Footer>
-        </>
-    );
+  return (
+    <>
+      <div className='bg-gray-100'>
+        <Navbar></Navbar>
+        {!isLoding ? (
+          <>
+            <WelcomeBanner
+              pageData={pageData.find((o) => o.role === "heading")}
+            ></WelcomeBanner>
+            <ShortBanner
+              pageData={pageData.find((o) => o.role === "topfeature")}
+            ></ShortBanner>
+            <Slider></Slider>
+            <br />
+            <br />
+            <Bannar
+              pageData={pageData.find((o) => o.role === "secfeature")}
+            ></Bannar>
+            <Events state={state}></Events>
+          </>
+        ) : (
+          <Loading />
+        )}
+      </div>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Home;
