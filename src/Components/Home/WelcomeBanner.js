3bdbc8b6f5b34cbeae7dc18746a8e42571eee7ca
abduljabbar15 <abduljabber1532002@gmail.com>
2023-01-21 12:49:16 +0600

    ..

--- src/Components/Home/WelcomeBanner.js
+++ src/Components/Home/WelcomeBanner.js
@@ -1,17 +1,17 @@
-import React from 'react'; 
+import React from 'react';
 import { Link } from 'react-router-dom';
- 
-const WelcomeBanner = ({ pageData }) => { 
+
+const WelcomeBanner = ({ pageData }) => {
+    console.log(process.env)
     return (
         <>
             <div className='flex flex-col relative z-20 items-center justify-center text-white '>
                 <div className="container  absolute z-30">
                     <h2 className='  md:text-5xl text-2xl  font-semibold md:text-left text-center'>{pageData.title}</h2>
                     <p className='md:text-2xl font-light text-md md:mb-4'>{pageData.descreption}</p>
-
                     <Link to={'/course'} className='text-blue-500 py-2 px-4 uppercase font-medium '>Find your department</Link>
                 </div>
-                <img src={process.env.REACT_APP_ROOT_URL+'/'+pageData.headingBackground} className="w-full opacity-80" alt="" />
+                <img src={process.env.REACT_APP_ROOT_URL + '/' + pageData.headingBackground} className="w-full opacity-80" alt="" />
             </div>
         </>
     );
